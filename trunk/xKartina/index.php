73c6a2e896f9fb0dd5ea99f88d47ef9307a3de1d
jkrapp <jkrapp@c66f9bad-357e-e763-2b56-13cc1f03ae27>
2010-06-06 06:53:49 +0000

    Version 0.1

---
+++ trunk/xKartina/index.php
@@ -0,0 +1,20 @@
+<?php
+	$content  = '<?xml version="1.0" encoding="UTF-8"?><url url="http/ts://217.19.223.4:28006/?ticket=Im8LyDysSO4Oo5RUduRIDEkeoWlJgQfBWtjRb0TJpcI%2FNYWszO%2BTCnHNoTnkSUwyJ8HO5uptqk6dj5oqo9m33TKEauLg%2FtRwJoVMdSVM8NeBQJN9sLZn5p6uVwzJ0tzT2X4xT4yCRRgR%2FJVvljqzqAA%2FnuQ18%2BJjchFpIfN4WxM%3D :http-caching=3000 :no-http-reconnect" />';
+	ereg('://([^" ]*)', $content, $regs);
+
+	#hack - remove url parts after space
+	#todo: chaneg regexp
+	$parts = explode(' ',$regs[1]);  
+	$url = $parts[0];
+	
+?>
+<html>
+<body>
+<a href = "cathegoryList.php">cathegoryList.php</a>
+<a href = "channelsList.php">channelsList.php</a>
+<a href = "showCathegory.php?id=1">Cathegory 1</a>
+<a href = "playChannel.php?d=0&id=7&number=1&title=%D0%A0%D0%B5%D0%BD%D0%A2%D0%92&vid=1">RenTv</a>
+
+<a href = "playlist.php">playlist</a>
+
+</body></html>
---
+++ trunk/xKartina/showCathegory.php
@@ -0,0 +1,60 @@
+<?php
+require_once "config.inc";
+require_once "rssTools.inc";
+require_once "tools.inc";
+#require_once "uft8_tools.inc";
+require_once "channelsParser.inc";
+require_once "ktvFunctions.inc";
+
+
+
+displayRssHeader("xTreamer chanel list for Kartina.TV");
+
+# decide whether chanels list update is needed
+if (! isset($_SESSION['channelsList']) || ! isset($_SESSION['lastUpdate']) ||
+    NOW_TIME - $_SESSION['lastUpdate'] > CL_UPDATE_INTERVAL) 
+{
+    # renew the list using existing cookie
+    $ktvFunctions = new KtvFunctions();
+    $rawList = $ktvFunctions->getChannelsList();
+
+    # remember new state
+    $_SESSION['channelsList'] = $rawList;
+    $_SESSION['lastUpdate'] = NOW_TIME;
+} else {
+    # use remembered list
+    $rawList = $_SESSION['channelsList'];
+}
+
+# parse raw list into prepared class hierarchy
+$channelsParser = new EregChannelsParser();
+$channelsParser->parse($rawList);
+$channelsParser->selectedChannel = $_SESSION['selectedChannel'];
+
+
+
+$itemsToDisplay     = array(); # itmes to be displayed on page
+$firstChannelNumber = 0;       # first displayed channel on page
+$lastChannelNumber  = 0;       # last displayed channel on page
+$currentEntry       = 0;
+$foundSelected      = false;
+$limit = CL_ITEMS_PER_PAGE;
+$cathegoryId = $_GET['id'];
+foreach ($channelsParser->categories as $category) {
+//    $itemsToDisplay[] = $category;
+//    $currentEntry++;
+    if($category->id == $cathegoryId) {
+	    foreach ($category->channels as $channel) {
+	        $itemsToDisplay[] = $channel;
+	        $currentEntry++;
+	    }
+    }
+}
+# display collected items
+foreach ($itemsToDisplay as $item) {
+	
+    echo   displayChannel($item);
+	
+}
+displayRssFooter();
+?>
