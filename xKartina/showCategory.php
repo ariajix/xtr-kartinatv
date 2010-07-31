b532d6a574105db855e238e6f8aea6c51c4e2f8d
jkrapp <jkrapp@c66f9bad-357e-e763-2b56-13cc1f03ae27>
2010-07-31 10:41:24 +0000

    first Working  version for Firmware 2.4

--- xKartina/showCategory.php
+++ xKartina/showCategory.php
@@ -2,13 +2,9 @@
 require_once "config.inc";
 require_once "rssTools.inc";
 require_once "tools.inc";
-#require_once "uft8_tools.inc";
+require_once "uft8_tools.inc";
 require_once "channelsParser.inc";
 require_once "ktvFunctions.inc";
-
-
-
-displayRssHeader("xTreamer chanel list for Kartina.TV");
 
 # decide whether chanels list update is needed
 if (! isset($_SESSION['channelsList']) || ! isset($_SESSION['lastUpdate']) ||
@@ -27,38 +23,20 @@ if (! isset($_SESSION['channelsList']) || ! isset($_SESSION['lastUpdate']) ||
 }
 
 # parse raw list into prepared class hierarchy
-$channelsParser = new EregChannelsParser();
+$channelsParser = new ChannelsParser();
 $channelsParser->parse($rawList);
 #$channelsParser->selectedChannel = $_SESSION['selectedChannel'];
 
-
-
 $itemsToDisplay     = array(); # itmes to be displayed on page
-//$firstChannelNumber = 0;       # first displayed channel on page
-//$lastChannelNumber  = 0;       # last displayed channel on page
-//$currentEntry       = 0;
-//$foundSelected      = false;
-//$limit = CL_ITEMS_PER_PAGE;
+
 $categoryId = $_GET['id'];
 foreach ($channelsParser->categories as $category) {
-//    $itemsToDisplay[] = $category;
-//    $currentEntry++;
     if($category->id == $categoryId || $categoryId == null) {
-	    foreach ($category->channels as $channel) {
-	    	if(null != $category->color) {
-	    		$channel->color = $category->color;
-	    	}
-	        $itemsToDisplay[] = $channel;
-//	        $currentEntry++;
-	    }
+    	$itemsToDisplay[] = $category;
     }
 }
+
 # display collected items
-$cnt=1;
-for($i=0;$i<$cnt;$i++) {
-foreach ($itemsToDisplay as $item) {
-    echo   displayChannel($item);
-}
-}
-displayRssFooter();
+echo drawChannelListTemplate($itemsToDisplay);
+
 ?>
